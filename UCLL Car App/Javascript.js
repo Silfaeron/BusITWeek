6ba40835b6ded1ffe125bc7e68b94cd43b6f9db2
Egonmalfroy <egon.malfroy@gmail.com>
2017-03-29 16:10:58 +0200

    Add files via upload

--- UCLL Car App/Javascript.js
+++ UCLL Car App/Javascript.js
@@ -5,7 +5,7 @@ var manufacturing = function () {
     var $manDiv = $('#manufacturing');
     var stringBuilder = '';
     stringBuilder += '<h2>Manufacturing</h2>';
-    stringBuilder = '<form id="manufacturingForm" class="form-group">';
+    stringBuilder += '<form id="manufacturingForm" class="form-group">';
     stringBuilder += '<label for="carId">Car id:</label>';
     stringBuilder += '<input type="text" id="carId" class="form-control">';
     stringBuilder += '<label for="brand">Brand:</label>';
@@ -47,24 +47,15 @@ var garage = function () {
     var $manDiv = $('#garage');
     var stringBuilder = '';
     stringBuilder += '<h2>garage</h2>';
-    stringBuilder = '<form id="garageForm" class="form-group">';
-    //stringBuilder+= '<button data-toggle="collapse" data-target="#repairs">Collapsible</button>';
-    //stringBuilder += '<div id="repairs" class="collapse">';
-    //var stringb = '';
+    stringBuilder += '<form id="garageForm" class="form-group">';
     stringBuilder += '<label for="carRepairs">Description of car repair:</label>';
     stringBuilder += '<textarea rows="4"></textarea>';
     stringBuilder += '<label for="repairDate">Date of repair:</label>';
     stringBuilder += '<input type="date" class="form-control" id="repairDate">';
-    //$('#repairs').append(stringb);
-    //stringBuilder += '</div>';
-
-    //stringBuilder+= '<button data-toggle="collapse" data-target="#maintinance">Collapsible</button>';
-    //stringBuilder += '<div id="maintinance" class="collapse">';
     stringBuilder += '<label for="carMaintenance">Description of car maintenance:</label>';
     stringBuilder += '<textarea rows="4"></textarea>';
     stringBuilder += '<label for="maintenanceDate">Date of maintenance:</label>';
     stringBuilder += '<input type="date" class="form-control" id="maintenanceDate">';
-    //stringBuilder += '</div>';
 
     stringBuilder += '<label for="brand">Brand:</label>';
     stringBuilder += '<select class="form-control" id="brand">';
@@ -86,6 +77,99 @@ var garage = function () {
     $manDiv.append(stringBuilder);
 };
 
+var vendor = function () {
+    hideAll();
+    var $manDiv = $('#vendor');
+    var stringBuilder = '';
+    stringBuilder += '<h2>vendor</h2>';
+    stringBuilder += '<form id="vendorForm" class="form-group">';
+    stringBuilder += '<label for="carMaintenance">Description of car maintenance:</label>';
+    stringBuilder += '<textarea rows="4"></textarea>';
+    stringBuilder += '<label for="maintenanceDate">Date of maintenance:</label>';
+    stringBuilder += '<input type="date" class="form-control" id="maintenanceDate">';
+
+    stringBuilder += '<label for="brand">Brand:</label>';
+    stringBuilder += '<select class="form-control" id="brand">';
+    stringBuilder += '<option>BMW</option>' +
+        '<option>Chevrolet</option>' +
+        '<option>Citroën</option>' +
+        '<option>Ferrari</option>'+
+        '<option>Fiat</option>'+
+        '<option>Ford</option>'+
+        '<option>Honda</option>'+
+        '<option>Jaguar</option>'+
+        '<option>Lamborhini</option>'+
+        '<option>Mercedes-Benz</option>'+
+        '<option>Tesla</option>'+
+        '<option>Peugot</option>';
+    stringBuilder += '</select>';
+    stringBuilder += '<label for="km">Km:</label>';
+    stringBuilder += '<input type="number" id="km" class="form-control">';
+    stringBuilder += '<label for="fuel">Type of fuel:</label>';
+    stringBuilder += '<select class="form-control" id="fuel">';
+    stringBuilder += '<option>Premium unleaded petrol</option>'+
+        '<option>Super unleaded petrol</option>'+
+        '<option>Diesel</option>'+
+        '<option>LPG Autogas</option>'+
+        '<option>Biofuels (biodiesel and bioethanol)</option>';
+    stringBuilder += '</select>';
+    stringBuilder += '<label for="year">Year:</label>';
+    stringBuilder += '<input type="number" min="1900" id="year" placeholder="1900" class="form-control">';
+    stringBuilder += '<input type="submit" value="submit" class="btn btn-primary">';
+    stringBuilder += '</form>';
+    $manDiv.append(stringBuilder);
+};
+
+var carOwner = function () {
+    hideAll();
+    var $manDiv = $('#carOwner');
+    var stringBuilder = '';
+    stringBuilder += '<h2>car owner</h2>';
+    stringBuilder += '<form id="carOwnerForm" class="form-group">';
+    stringBuilder += '<label for="carRepairs">Description of car repair:</label>';
+    stringBuilder += '<textarea rows="4"></textarea>';
+    stringBuilder += '<label for="repairDate">Date of repair:</label>';
+    stringBuilder += '<input type="date" class="form-control" id="repairDate">';
+
+    stringBuilder += '<label for="carMaintenance">Description of car maintenance:</label>';
+    stringBuilder += '<textarea rows="4"></textarea>';
+    stringBuilder += '<label for="maintenanceDate">Date of maintenance:</label>';
+    stringBuilder += '<input type="date" class="form-control" id="maintenanceDate">';
+
+    stringBuilder += '<label for="brand">Brand:</label>';
+    stringBuilder += '<select class="form-control" id="brand">';
+    stringBuilder += '<option>BMW</option>' +
+        '<option>Chevrolet</option>' +
+        '<option>Citroën</option>' +
+        '<option>Ferrari</option>'+
+        '<option>Fiat</option>'+
+        '<option>Ford</option>'+
+        '<option>Honda</option>'+
+        '<option>Jaguar</option>'+
+        '<option>Lamborhini</option>'+
+        '<option>Mercedes-Benz</option>'+
+        '<option>Tesla</option>'+
+        '<option>Peugot</option>';
+    stringBuilder += '</select>';
+    stringBuilder += '<label for="manufacturerPlant">Manufacturer Plant:</label>';
+    stringBuilder += '<input type="text" id="manufacturerPlant" class="form-control">';
+    stringBuilder += '<label for="km">Km:</label>';
+    stringBuilder += '<input type="number" id="km" class="form-control">';
+    stringBuilder += '<label for="fuel">Type of fuel:</label>';
+    stringBuilder += '<select class="form-control" id="fuel">';
+    stringBuilder += '<option>Premium unleaded petrol</option>'+
+        '<option>Super unleaded petrol</option>'+
+        '<option>Diesel</option>'+
+        '<option>LPG Autogas</option>'+
+        '<option>Biofuels (biodiesel and bioethanol)</option>';
+    stringBuilder += '</select>';
+    stringBuilder += '<label for="year">Year:</label>';
+    stringBuilder += '<input type="number" min="1900" id="year" placeholder="1900" class="form-control">';
+    stringBuilder += '<input type="submit" value="submit" class="btn btn-primary">';
+    stringBuilder += '</form>';
+    $manDiv.append(stringBuilder);
+};
+
 var login = function () {
     hideAll();
     var stringBuilder = '';
@@ -95,9 +179,10 @@ var login = function () {
     stringBuilder += '<div class="account-wall">';
     stringBuilder += '<img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120" alt="">';
     stringBuilder += '<form class="form-signin">';
-    stringBuilder += '<input type="text" class="form-control" placeholder="Email" required autofocus>';
-    stringBuilder += '<input type="password" class="form-control" placeholder="Password" required>';
-    stringBuilder += '<button id="signIn" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>';
+    stringBuilder += '<input id="name" type="text" class="form-control" placeholder="Name" required autofocus>';
+    stringBuilder += '<input id="email" type="email" class="form-control" placeholder="Email" required>';
+    stringBuilder += '<input id="password" type="password" class="form-control" placeholder="Password" required>';
+    stringBuilder += '<input id="signIn" class="btn btn-lg btn-primary btn-block" type="submit" value="Sign in"/>';
     stringBuilder += '<label class="checkbox pull-left">';
     stringBuilder += '<input type="checkbox" value="remember-me">Remember me</label>';
     stringBuilder += '<a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>';
@@ -118,11 +203,11 @@ var register = function () {
     stringBuilder += '<div class="account-wall">';
     stringBuilder += '<img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120" alt="">';
     stringBuilder += '<form class="form-signin">';
-    stringBuilder += '<input type="text" class="form-control" placeholder="Name" required autofocus>';
-    stringBuilder += '<input type="text" class="form-control" placeholder="Email" required>';
-    stringBuilder += '<input type="password" class="form-control" placeholder="Password" required>';
+    stringBuilder += '<input id="name" type="text" class="form-control" placeholder="Name" required autofocus>';
+    stringBuilder += '<input id="email" type="text" class="form-control" placeholder="Email" required>';
+    stringBuilder += '<input id=password" type="password" class="form-control" placeholder="Password" required>';
     stringBuilder += '<input type="password" class="form-control" placeholder="Password again" required>';
-    stringBuilder += '<button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>';
+    stringBuilder += '<button id="signUp" class="btn btn-lg btn-primary btn-block" type="submit">Register</button>';
     stringBuilder += '<label class="checkbox pull-left">';
     stringBuilder += '<input type="checkbox" value="remember-me">Accept the terms of agreement</label>';
     stringBuilder += '</form>';
@@ -138,13 +223,65 @@ var hideAll = function () {
     $('#login').html('');
     $('#garage').html('');
     $('#manufacturing').html('');
+    $('#vendor').html('');
+    $('#carOwner').html('');
+};
+
+var progressLogin = function () {
+    var account = {};
+    account.name = $('#login #name').val();
+    account.email = $('#login #email').val();
+    account.password = $('#login #password').val();
+    console.log(account.email);
+
+    switch (account.name){
+        case 'manufacturing':
+            manufacturing();
+            break;
+        case 'garage':
+            garage();
+            break;
+        case 'carowner':
+            carOwner();
+            break;
+        case 'vendor':
+            vendor();
+            break;
+    }
+
+    if(account.name == 'manufacturing'){
+        manufacturing();
+    }
+    saveAccount(account);
+    displayLoggedInUser();
+
+};
+
+var displayLoggedInUser = function () {
+    if((typeof Storage)  !== void(0)) {
+        var loggedInUser = JSON.parse(localStorage.getItem('account'));
+        if( loggedInUser  !== null) {
+            console.log('header p ding');
+            $('header a').html('');
+            $('header a').append(loggedInUser.email)
+        }
+    }
+};
+
+var saveAccount = function(account) {
+    console.log('saveAccount');
+    if ((typeof Storage) !== void(0)) {
+        localStorage.setItem('account', JSON.stringify(account));
+    }
 };
 
 $(function () {
     login();
     var $login = $('#login');
+    var $register = $('#register');
     $login.on('click', '#createAcc', register);
-    $login.on('click', '#signIn', manufacturing);
-    $('#register').on('click', '#backToLogin', login);
+    $login.on('submit', progressLogin);
+    $register.on('click', '#backToLogin', login);
+
 });
 
