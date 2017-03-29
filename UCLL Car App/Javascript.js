86bd55af454e57a0a873b01db7f036c0ad6a90f1
Egonmalfroy <egon.malfroy@gmail.com>
2017-03-29 11:24:10 +0200

    Add files via upload

---
+++ UCLL Car App/Javascript.js
@@ -0,0 +1,15 @@
+
+
+var manufacturing = function () {
+    var $manDiv = $('#manufacturing');
+    var stringBuilder = '<form id="manufacturingForm" class="form-group"';
+    stringBuilder += '<label for="carId">Car id:</label>';
+    stringBuilder += '<input type="text" id="carId" class="form-control">';
+    stringBuilder += '<label for="brand">Brand:</label>';
+    stringBuilder += '<select class="form-control" id="brand">';
+    stringBuilder +=
+};
+
+$(function () {
+    manufacturing();
+});
